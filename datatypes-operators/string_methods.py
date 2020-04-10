f0ba417446e748ff94c6dc2708df703240148f53
Ravi <ravinder9.reddy@gmail.com>
2020-04-10 20:03:51 +0100

    learn datatypes

---
+++ datatypes-operators/string_methods.py
@@ -0,0 +1,29 @@
+# method - A function that belongs to an object.
+
+print("sebastian thrun".title())  # first letter of each word is capitalized.
+
+full_name = "sebastian thrun"
+print(full_name.islower())  # islower() method checks one of the characters in a string are lowercase.
+
+fishes = "One fish, two fish, red fish, blue fish"
+count_fishes = fishes.count('fish')  # count() counts the number of 'fish' words in fishes
+print(count_fishes)
+
+print(len("this"))
+
+type(12)
+
+print("Hello world")
+
+print("Charvi has {} baloons".format(100))
+
+animal = "Dog"
+action = "bite"
+print("Does your {} {}?".format(animal, action))
+
+new_str = "The cow jumped over the moon."
+print(new_str.split())
+print(new_str.split(' ', 3))
+print(new_str.split('.'))
+print(new_str.split(None, 3))
+
