b1ed96fc7a2eee93abdfda520b5722b1c48f1fe2
Ravi <ravinder9.reddy@gmail.com>
2020-04-12 05:43:20 +0100

    add python methods

---
+++ datatypes-operators/shortcuts.py
@@ -0,0 +1,12 @@
+# countries and population (in millions)
+countries = {'India': 1353,
+             'USA': 328.2,
+             'UK': 66.65,
+             'China': 1393}
+
+print(type(countries))
+print(dir(type(countries)))
+
+print(type(countries['India']))
+print(type(countries['UK']))
+
