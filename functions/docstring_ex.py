5823bd4d91731f4ea03a83d565331a9b24902c26
Ravi <ravinder9.reddy@gmail.com>
2020-04-13 20:03:03 +0100

    add docstring

---
+++ functions/docstring_ex.py
@@ -0,0 +1,11 @@
+def population_density(population, land_area):
+    """Calculate the population density of an area.
+    INPUT:
+    population: int. The population of the area.
+    land_area: int or float. This function is unit-agnostic, if you pass in values in
+    terms of square km or square miles the function will return density in those units.
+    OUTPUT:
+    population_density: population/land_area. The population density of a particular area.
+    """
+    return population / land_area
+
